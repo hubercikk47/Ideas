72498173d828da41f55e3cfe824abddb1b7df3c0
hubercikk47 <hubert.niespial@onet.pl>
2025-02-04 15:17:35 +0100

    Not done yet

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -85,9 +85,12 @@ WSGI_APPLICATION = 'djangoProject1.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "my_service",
+            "passfile": ".my_pgpass",
+        },
     }
 }
 
